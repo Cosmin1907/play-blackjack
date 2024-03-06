af3ad10dd89322ddca25e45cb94ec08be858ee3d
Cosmin Timofte <timoftecosmin@yahoo.com>
2024-03-06 15:54:10 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Display welcome Message
+
+# Initialize Deck
+
+# Shuffle Deck
+
+# Deal Cards
+
+# Calculate Hand Value
+
+# Hit
+
+# Stand
+
+# Check Bust
+
+# Determine Winner
+
+# Display Hands
+
+# Update Bankroll
