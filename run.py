bde487df3e6da1cfa881310825c4a759fe1514de
Cosmin Timofte <timoftecosmin@yahoo.com>
2024-03-07 10:02:58 +0000

    Implement card deck functionality with multiple decks option

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 import pyfiglet
+import random
+import copy
 
 # Display welcome Message
 result = pyfiglet.figlet_format("Blackjack", font = "digital")
 print(result)
 
 # Initialize Deck
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+one_deck = 4 * cards
+decks = 4
+game_deck = copy.deepcopy(decks * one_deck)
+print(game_deck)
 
 # Shuffle Deck
 
