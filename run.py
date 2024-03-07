a28b64030b0001ad5152d3f730cc8ea73f06581e
Cosmin Timofte <timoftecosmin@yahoo.com>
2024-03-07 06:30:06 +0000

    Add pyfiglet module and inplement initial message display

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pyfiglet
 
 # Display welcome Message
+result = pyfiglet.figlet_format("Blackjack", font = "digital")
+print(result)
 
 # Initialize Deck
 
