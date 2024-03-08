da33f54899188fbef9f7d43dfa3768c54ecb7c75
Cosmin Timofte <timoftecosmin@yahoo.com>
2024-03-08 15:59:36 +0000

    Implement basic card dealing functionality

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import pyfiglet
 import random
 import copy
 
+
 # Display welcome Message
 result = pyfiglet.figlet_format("Blackjack", font = "digital")
 print(result)
@@ -9,13 +10,24 @@ print(result)
 # Initialize Deck
 cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 one_deck = 4 * cards
-decks = 4
+decks = 1
 game_deck = copy.deepcopy(decks * one_deck)
 print(game_deck)
 
-# Shuffle Deck
+# General variables
+records = [0, 0, 0]
+player_score = 0
+dealer_score = 0
+player_hand = []
+dealer_hand = []
 
 # Deal Cards
+def deal_cards(turn):
+    card = random.choice(game_deck)
+    turn.append(card)
+    game_deck.remove(card)
+    print(card)
+
 
 # Calculate Hand Value
 
@@ -29,4 +41,10 @@ print(game_deck)
 
 # Display Hands
 
+# Game loop
+
 # Update Bankroll
+
+deal_cards(player_hand)
+deal_cards(dealer_hand)
+print(game_deck)
