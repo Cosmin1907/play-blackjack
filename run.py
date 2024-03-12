08f403b9edf35830c98aa19508b8b3ddeb5eae9f
Cosmin Timofte <timoftecosmin@yahoo.com>
2024-03-12 10:45:21 +0000

    Implementing basic game conditions and game loop

--- run.py
+++ run.py
@@ -2,6 +2,15 @@ import pyfiglet
 import random
 import copy
 
+# Global variables
+records = [0, 0, 0]
+player_score = 0
+dealer_score = 0
+player_hand = []
+dealer_hand = []
+playerIn = True
+dealerIn = True
+
 
 # Display welcome Message
 result = pyfiglet.figlet_format("Blackjack", font = "digital")
@@ -12,21 +21,14 @@ cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 one_deck = 4 * cards
 decks = 1
 game_deck = copy.deepcopy(decks * one_deck)
-print(game_deck)
-
-# General variables
-records = [0, 0, 0]
-player_score = 0
-dealer_score = 0
-player_hand = []
-dealer_hand = []
+#print(game_deck)
 
 # Deal Cards
 def deal_cards(turn):
     card = random.choice(game_deck)
     turn.append(card)
     game_deck.remove(card)
-    print(card)
+    return card
 
 
 # Calculate Hand Value
@@ -42,16 +44,9 @@ def total(turn):
                 total += 1
             else:
                 total += 11
-    print(total)
+    return total
 
 
-# Hit
-
-# Stand
-
-# Check Bust
-
-# Determine Winner
 # Display Hands
 def show_hand():
     if len(dealer_hand) == 2:
@@ -64,11 +59,49 @@ for _ in range(2):
     deal_cards(dealer_hand)
     deal_cards(player_hand)
 
-print(dealer_hand)
-print(player_hand)
+while playerIn or dealerIn:
+    print(f"Dealer hand: {show_hand()} and X")
+    print(f"Your hand: {player_hand} for a total of {total(player_hand)}")
+    if playerIn: 
+        stay_hit = input("Press the key\n1 to Stay\n2 to Hit\n")
+    if stay_hit == '1':
+        playerIn = False
+    else: 
+        deal_cards(player_hand)
+    if total(dealer_hand) > 16:
+        dealerIn = False
+    else:
+        deal_cards(dealer_hand)
+    if total(player_hand) >= 21:
+        break
+    elif total(dealer_hand) >= 21:
+        break
+
+# Determine Winner
+if total(player_hand) == 21:
+    print(f"\nYou have {player_hand} for a total of {total(player_hand)}")
+    print("Blackjack!")
+elif total(dealer_hand) == 21:
+    print(f"\nDealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("Blackjack!")
+elif total(player_hand) == 21 and total(dealer_hand) == 21:
+    print("Tye Game!")
+elif total(player_hand) > 21:
+    print(f"\nYou have {player_hand} for a total of {total(player_hand)}")
+    print(f"\nDealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("You bust! Dealer Wins!")
+elif total(dealer_hand) > 21:
+    print(f"\nYou have {player_hand} for a total of {total(player_hand)}")
+    print(f"\nDealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("Dealer busts! You Win")
+elif total(dealer_hand) > total(player_hand):
+    print(f"\nYou have {player_hand} for a total of {total(player_hand)}")
+    print(f"\nDealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("Dealer Wins!")
+elif total(dealer_hand) < total(player_hand):
+    print(f"\nYou have {player_hand} for a total of {total(player_hand)}")
+    print(f"\nDealer has {dealer_hand} for a total of {total(dealer_hand)}")
+    print("You Win!")
+
 
 # Update Bankroll
-
-
-total(dealer_hand)
-total(player_hand)
